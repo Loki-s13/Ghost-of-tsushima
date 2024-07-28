5acb125473984e4e0d7387c159bee63b2ef94f18
Loki-sl13 <loki.sl1133@gmail.com>
2024-07-28 01:39:33 -0400

    fixed process id function. Added basee address function

--- src/cheat.rs
+++ src/cheat.rs
@@ -1,35 +1,49 @@
+use std::ffi::OsString;
+use std::os::windows::ffi::OsStringExt;
+use windows::Win32::System::Threading::{OpenProcess, PROCESS_VM_OPERATION, PROCESS_VM_WRITE};
 use windows::Win32::Foundation::CloseHandle;
 use windows::Win32::System::Diagnostics::Debug::WriteProcessMemory;
-use windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32};
+use windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, Process32FirstW, Process32NextW, PROCESSENTRY32W, TH32CS_SNAPPROCESS};
 
-fn process_id(process_name: &str) -> Option<u32> {
+pub fn process_id(process_name: &str) -> Result<u32, Box<dyn std::error::Error>> {
     unsafe{
-        let snapshot = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 );
+        let snapshot = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 )?;
         if snapshot.is_invalid() {
-            println!("Invalid!");
-            return None;
+            return Err(anyhow!("Failed to create snapshot").into());
         } 
-        let mut pe32 = unsafe { std::mem::zeroed::<PROCESSENTRY32>() };
-        pe32.dwSize = std::mem::size_of::<PROCESSENTRY32>() as u32;
+        let mut pe32 = std::mem::zeroed::<PROCESSENTRY32W>();
+        pe32.dwSize = std::mem::size_of::<PROCESSENTRY32W>() as u32;
 
-        if Process32First(snapshot, &mut pe32).as_bool() {
+        Process32FirstW(snapshot, &mut pe32)?;
+
             loop {
-                if process_name == pe32.szExeFile.to_string_lossy() {
-                    CloseHandle(snapshot);
-                    return Some(pe32.th32ProcessID);
-                }
-
-                if !Process32Next(snapshot, &mut pe32).as_bool() {
+                if Process32NextW(snapshot, &mut pe32).is_err() {
                     break;
                 }
+                let string = OsString::from_wide(&pe32.szExeFile[..pe32.szExeFile.iter().position(|c| *c==0u16).unwrap()]);            
+                if process_name == string {
+                    let _ = CloseHandle(snapshot);
+                    return Ok(pe32.th32ProcessID);
+                }
             }
-        }
-        CloseHandle(snapshot);
-        None
+    
+
+       let _ = CloseHandle(snapshot);
+        
     }
+    Ok(0)
 }
 
-fn infinite_arrows(){
-    //let process = unsafe {OpenProcess(PROCESS_VM_WRITE, false, )};
+pub fn Base_Address(){
+    
+}
 
+
+fn infinite_arrows() -> Result<(), Box<dyn std::error::Error>>{
+    let pid = process_id("GhostOfTsushima.exe")?;
+
+    let process = unsafe {OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION, false, pid)}?;    
+
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
+use cheat::process_id;
+
 mod cheat;
+
+#[macro_use]
+extern crate anyhow;
+
+fn main() {
+    let test = process_id("AdjustService.exe").unwrap();
+    println!("Process ID: {}", test);
+}
